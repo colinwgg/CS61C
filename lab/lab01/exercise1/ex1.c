6702200970db5b55f3697aa728839112a8886bfb
Colin Wang <2802235800@qq.com>
2023-06-10 23:12:33 +0800

    ex1 finished

--- lab/lab01/exercise1/ex1.c
+++ lab/lab01/exercise1/ex1.c
@@ -7,7 +7,13 @@ There are two different ways to iterate through a string.
 2nd way hint: all strings end in a null terminator */
 int num_occurrences(char *str, char letter) {
     /* TODO: implement num_occurrences */
-    return 0;
+    int times = 0;
+    for (int i = 0; i < strlen(str); i++) {
+        if (str[i] == letter) {
+            times++;
+        }
+    }
+    return times;
 }
 
 /* Populates DNA_SEQ with the number of times each nucleotide appears.
@@ -15,5 +21,9 @@ Each sequence will end with a NULL terminator and will have up to 20 nucleotides
 All letters will be upper case. */
 void compute_nucleotide_occurrences(DNA_sequence *dna_seq) {
     /* TODO: implement compute_nucleotide_occurrences */
+    dna_seq->A_count = num_occurrences(dna_seq->sequence, 'A');
+    dna_seq->G_count = num_occurrences(dna_seq->sequence, 'G');
+    dna_seq->C_count = num_occurrences(dna_seq->sequence, 'C');
+    dna_seq->T_count = num_occurrences(dna_seq->sequence, 'T');
     return;
 }
