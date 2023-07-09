8a494a7d290b4a996f2c095507b8f54eb04bc303
Colin Wang <2802235800@qq.com>
2023-07-09 22:02:56 +0800

    lab02 finished

--- lab/lab02/bit_ops.c
+++ lab/lab02/bit_ops.c
@@ -3,17 +3,19 @@
 
 /* Returns the Nth bit of X. Assumes 0 <= N <= 31. */
 unsigned get_bit(unsigned x, unsigned n) {
-    /* YOUR CODE HERE */
-    return -1; /* UPDATE WITH THE CORRECT RETURN VALUE*/
+    return (x & (1 << n)) >> n;
 }
 
 /* Set the nth bit of the value of x to v. Assumes 0 <= N <= 31, and V is 0 or 1 */
 void set_bit(unsigned *x, unsigned n, unsigned v) {
-    /* YOUR CODE HERE */
+    unsigned mark = 1 << n;
+    mark = ~mark;
+    *x &= mark;
+    *x |= v << n;
 }
 
 /* Flips the Nth bit in X. Assumes 0 <= N <= 31.*/
 void flip_bit(unsigned *x, unsigned n) {
-    /* YOUR CODE HERE */
+    *x ^= 1 << n;
 }
 
