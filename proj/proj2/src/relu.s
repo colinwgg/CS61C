2dec0c1dc842ce3aeb51eddd8b7aea641ded02ca
Colin Wang <2802235800@qq.com>
2023-09-25 13:37:32 +0800

    finished partA task2

--- proj/proj2/src/relu.s
+++ proj/proj2/src/relu.s
@@ -12,27 +12,44 @@
 #   - If the length of the array is less than 1,
 #     this function terminates the program with error code 36
 # ==============================================================================
+# void relu(int *a0, int a1) {
+#   for (int i = 0, i < a1, i++) {
+#       #if (a0[i] < 0) {
+#           a0[i] = 0;
+#           }
+#       }
+# }
 relu:
     # Prologue
+    li t0, 1
+    bge a1, t0, prepare
+    li a0, 36
+    j exit
 
+prepare:
+    addi sp, sp, -8
+    sw ra, 0(sp)
+    sw s0, 4(sp)
+    li t0, 0 # i = 0
 
 loop_start:
-
-
-
-
-
-
-
+    bge t0, a1, loop_end
+    slli s0, t0, 2
+    add t1, s0, a0
+    lw t2, 0(t1)
+    blt t2, x0, loop_continue
+    addi t0, t0, 1
+    j loop_start
 
 loop_continue:
-
-
+    bge t0, a1, loop_end
+    sw x0, 0(t1)
+    addi t0, t0, 1
+    j loop_start
 
 loop_end:
-
-
     # Epilogue
-
-
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    addi sp, sp, 8
     jr ra
